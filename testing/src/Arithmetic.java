ba69ba20d04bcc9a945c8c15543ca45c033f50c8
Roma <andronic165@gmail.com>
2021-02-01 13:24:17 +0200

    Exercitiu aritmetic

--- testing/src/Arithmetic.java
+++ testing/src/Arithmetic.java
@@ -1,7 +1,17 @@
 import java.util.Scanner;
 
 public class Arithmetic {
-    Scanner input = new Scanner(System.in);
-    int number1;
-    int number2;
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int number1;
+        int number2;
+        int sum;
+
+        number1 = input.nextInt();
+
+        number2 = input.nextInt();
+
+        sum = number1 + number2;
+    }
 }
