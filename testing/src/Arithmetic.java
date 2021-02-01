3ee92f01e14db6433784ed5a5c9382adf686c2bc
Roma <andronic165@gmail.com>
2021-02-01 13:29:55 +0200

    Exercitiu aritmetic

--- testing/src/Arithmetic.java
+++ testing/src/Arithmetic.java
@@ -7,13 +7,16 @@ public class Arithmetic {
         int number1;
         int number2;
         int sum;
+        double quot;
 
         number1 = input.nextInt();
 
         number2 = input.nextInt();
 
         sum = number1 + number2;
+        quot = number1 / (double)number2;
 
         System.out.printf("Suma este %d%n",sum);
+        System.out.printf("Rezultatul impartirii este %f%n",quot);
     }
 }
